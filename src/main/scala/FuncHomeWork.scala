f1fd40c0a2ef32585f13cc73c8aa2ed9bbd31278
fettqa <fettqa@gmail.com>
2019-10-30 13:41:37 +0300

    v2

--- src/main/scala/FuncHomeWork.scala
+++ src/main/scala/FuncHomeWork.scala
@@ -1,15 +1,15 @@
 object FuncHomeWork extends App {
-    """частичное применение"""
-  def partApply(f: (Int,Int) => Int, x: Int): Int => Int = f(100,_) + x
-  def calc50(i:Int, j: Int):Int = i + j
-  println(partApply(calc50,0)(1))
-  """композиции"""
+    /*частичное применение*/
+  def partApply(f: (Int,Int) => Int, x: Int): Int => Int = f(_,4) - x
+  val partial = partApply(_ + _,2)
+  val result = partial(3)
+  println(result)
+  /*композиции*/
   def f(x:Int): Double = x.toDouble
   def g(y:Double): String = y.toString
   def fg = g _ compose f
-  def gf = f _ andThen g
-  println(fg(2),gf(2))
-  """каррирование"""
+  println(fg(2))
+  /*каррирование*/
   def curry(s:(Int,Double) => String):Int => Double => String = s.curried
   def s(s1: Int,s2: Double): String = s1.toString + " + " + s2.toString
   println(curry(s)(3)(3))
